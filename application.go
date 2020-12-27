6b04370702e35bfb4c78ae50d71e2101bd3a654c
mls-361 <mls-361@protonmail.com>
2020-12-27 17:51:16 +0100

    En cours de développement

---
+++ application.go
@@ -0,0 +1,108 @@
+/*
+------------------------------------------------------------------------------------------------------------------------
+####### application ####### (c) 2020-2021 mls-361 ################################################## MIT License #######
+------------------------------------------------------------------------------------------------------------------------
+*/
+
+package application
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/mls-361/fqdn"
+	"github.com/mls-361/util"
+	"github.com/mls-361/uuid"
+)
+
+type (
+	// Application AFAIRE.
+	Application struct {
+		id        string
+		name      string
+		version   string
+		builtAt   time.Time
+		startedAt time.Time
+		host      string
+		devel     bool
+	}
+)
+
+// New AFAIRE.
+func New(name, version, builtAt string) *Application {
+	t, _ := util.EpochStrToTime(builtAt, 0)
+
+	app := &Application{
+		id:        uuid.New(),
+		name:      name,
+		version:   version,
+		builtAt:   t,
+		startedAt: time.Now(),
+	}
+
+	return app
+}
+
+// ID AFAIRE.
+func (a *Application) ID() string { return a.id }
+
+// Name AFAIRE.
+func (a *Application) Name() string { return a.name }
+
+// Version AFAIRE.
+func (a *Application) Version() string { return a.version }
+
+// BuiltAt AFAIRE.
+func (a *Application) BuiltAt() time.Time { return a.builtAt }
+
+// StartedAt AFAIRE.
+func (a *Application) StartedAt() time.Time { return a.startedAt }
+
+// Host AFAIRE.
+func (a *Application) Host() string { return a.host }
+
+// Devel AFAIRE.
+func (a *Application) Devel() bool { return a.devel }
+
+// Initialize AFAIRE.
+func (a *Application) Initialize() error {
+	s, ok := os.LookupEnv(strings.ToUpper(a.name) + "_DEVEL")
+	if ok {
+		devel, err := strconv.ParseBool(s)
+		if err != nil {
+			return err
+		}
+
+		a.devel = devel
+	}
+
+	fqdn, err := fqdn.FQDN()
+	if err != nil {
+		return err
+	}
+
+	a.host = fqdn
+
+	return nil
+}
+
+// OnError AFAIRE.
+func (a *Application) OnError(err error) error {
+	fmt.Fprintf( ///////////////////////////////////////////////////////////////////////////////////////////////////////
+		os.Stderr,
+		"Error: application=%s version=%s builtAt=%s >>> %s\n",
+		a.name,
+		a.version,
+		a.builtAt.String(),
+		err,
+	)
+
+	return err
+}
+
+/*
+######################################################################################################## @(°_°)@ #######
+*/
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/mls-361/application
+
+go 1.15
+
+require (
+	github.com/mls-361/fqdn v0.0.0-20201227155850-c41b764312fc
+	github.com/mls-361/util v0.0.0-20201227162418-87a64a61f1b6
+	github.com/mls-361/uuid v0.0.0-20201227141247-b31d88caee2c
+)
